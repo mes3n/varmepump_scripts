d00287aef40d8d96631f04d127b5c6f321e189a9
mes3n <skidmackan@gmail.com>
2022-10-06 00:17:36 +0200

    removed all traces of varmescript

--- run.sh
+++ run.sh
@@ -14,13 +14,12 @@ cd varmeweb
 webPID=$!
 cd ..
 
-echo "Running script with pid=$scriptPID and server with pid=$webPID."
+echo "Running server with pid=$webPID."
 
 safe_exit () {
 
-    echo "Stopping script with pid=$scriptPID and server with pid=$webPID."
+    echo "Stopping server with pid=$webPID."
 
-    kill $scriptPID || echo "$scriptPID is not running"
     kill $webPID || echo "$webPID is not running"
 
     exit 0
