07521bcd8eecbf4a46e2acb834682e28789c906b
mes3n <skidmackan@gmail.com>
2022-10-23 21:49:38 +0200

    changed file structure of install

--- install.sh
+++ install.sh
@@ -7,35 +7,39 @@ sudo apt -y upgrade
 
 sudo apt -y install git
 
-mkdir varmeweb
-git clone https://github.com/mes3n/varmeweb varmeweb
-sudo pip install -r varmeweb/requirements.txt
+server_dir = varmeserver
+mkdir $server_dir
+git clone https://github.com/mes3n/varmeserver $server_dir
+sudo pip install -r $server_dir/requirements.txt
 
-mkdir varmescript
-git clone https://github.com/mes3n/varmescript varmescript
-sudo pip install -r varmescript/requirements.txt
+script_dir = varmescript
+mkdir $script_dir
+git clone https://github.com/mes3n/varmescript $script_dir
+sudo pip install -r $script_dir/requirements.txt
+
+ln -s $script_dir $server_dir/$script_dir
 
 mkdir logs
-mkdir varmescript/config
-mkdir varmescript/database
+mkdir $script_dir/config
+mkdir $script_dir/database
 
-echo "[]" > varmescript/config/parameters.json
+echo "[]" > $script_dir/parameters.json
 
 echo "{
     \"TOKEN\": \"\",
     \"husdata_ip\": \"\",
     \"low_mid\": 1.0,
     \"mid_high\": 2.0
-}" > varmescript/config/config.json
+}" > $script_dir/config/config.json
 
-python setup_husdata_csv.py 192.168.1.194 || touch varmescript/database/husdata.csv
+python setup_husdata_csv.py 192.168.1.194 || touch $script_dir/database/husdata.csv
 
 echo "{
     \"script_dir\": \"$(realpath varmescript)\",
     \"script_config\": \"$(realpath varmescript/config/config.json)\",
     \"script_parameters\": \"$(realpath varmescript/config/parameters.json)\",
     \"script_database\": \"$(realpath varmescript/database/husdata.csv)\"
-}" > varmeweb/paths.json
+}" > $server_dir/paths.json
 
 (sudo crontab -l ; echo "@reboot sh $(pwd)/run.sh > $(pwd)/logs/varmepump.log 2>&1") | sudo crontab
 echo "Set up crontab for launching server on pi start."
